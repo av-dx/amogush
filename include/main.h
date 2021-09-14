ad8805861d7f7b18e652411bc2fbd126099e8c23
av-dx <aashwin.vaish@research.iiit.ac.in>
2021-09-14 12:38:24 +0530

    Move init() and builtin callback to main.h

---
+++ include/main.h
@@ -0,0 +1,21 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+typedef int(*cmd_callback)(int, char**);
+
+extern char **builtin_cmds;
+extern cmd_callback *builtin_cmd_callbacks;
+extern const unsigned int NUM_BUILTIN_CMDS;
+
+enum BuiltinsCMD {
+    BUILTIN_CD,
+    BUILTIN_LS,
+    BUILTIN_ECHO,
+    BUILTIN_LIST_START = 0,
+};
+
+void init();
+
+int builtin_cmd_callback(enum BuiltinsCMD cmdid, int argc, char **argv);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <common.h>
+#include <main.h>
 #include <prompt.h>
+#include <echo.h>
+#include <cd.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pwd.h>
+
+char **builtin_cmds;
+cmd_callback *builtin_cmd_callbacks;
+const unsigned int NUM_BUILTIN_CMDS = 3;
 
 int main() {
     init();
@@ -57,4 +67,48 @@ int main() {
         cmd_line = NULL;
     }
     return 0;
+}
+
+void init() {
+    // TODO: Add 4096 under assumptions
+    cwdpath = (char *)malloc(4096 * sizeof(char));
+    cwddisplay = (char *)calloc(4096, sizeof(char));
+    homepath = (char *)calloc(4096, sizeof(char));
+
+    getcwd(homepath, 4096);
+    homepath_len = strlen(homepath);
+
+    // TODO: Add 4096 under assumptions
+    hostname = (char *)calloc(64, sizeof(char));
+    if (gethostname(hostname, 64) == -1) {
+        perror("Unable to get host name");
+    } else {
+        hostname[63] = '\0';
+    }
+
+    errno = 0;
+    __uid_t userid = getuid();
+    struct passwd *pw = getpwuid(userid);
+    if (pw != NULL) {
+        int len = strlen(pw->pw_name);
+        username = (char *)calloc(len + 1, sizeof(char));
+        strcpy(username, pw->pw_name);
+    } else if (errno) {
+        perror("Unable to get user name");
+    } else {
+        printf("Unable to get user name : Unknown Error\n");
+    }
+
+    builtin_cmds = (char **)calloc(NUM_BUILTIN_CMDS + 1, sizeof(char *));
+    builtin_cmds[BUILTIN_CD] = "cd";
+    builtin_cmds[BUILTIN_ECHO] = "echo";
+    builtin_cmds[BUILTIN_LS] = "ls";
+
+    builtin_cmd_callbacks = malloc(NUM_BUILTIN_CMDS + 1 * sizeof(cmd_callback));
+    builtin_cmd_callbacks[BUILTIN_ECHO] = &echo;
+    builtin_cmd_callbacks[BUILTIN_CD] = &cd;
+}
+
+int builtin_cmd_callback(enum BuiltinsCMD cmdid, int argc, char **argv) {
+    return builtin_cmd_callbacks[cmdid](argc, argv);
 }
