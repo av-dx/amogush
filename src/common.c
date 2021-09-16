bb0529ab8bf187b4d4bdc4707df253dd97d48eb9
av-dx <aashwin.vaish@research.iiit.ac.in>
2021-09-16 22:47:23 +0530

    Remove unused var

--- src/common.c
+++ src/common.c
@@ -10,7 +10,6 @@ char *homepath;
 size_t homepath_len;
 
 int is_prefix(const char *pfx, const char *str) {
-    int p_pos = 0;
     size_t pfx_len = strlen(pfx);
     if (strncmp(pfx, str, pfx_len) == 0) {
         return 1;
